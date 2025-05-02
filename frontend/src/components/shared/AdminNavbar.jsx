9da2f36c81c5afe8a8fa15c726c3f612154245dd
nehala-t <nehalamehak2772@gmail.com>
2025-05-02 18:54:16 +0530

    style  sidebar

--- frontend/src/components/shared/AdminNavbar.jsx
+++ frontend/src/components/shared/AdminNavbar.jsx
@@ -23,21 +23,21 @@ const AdminNavbar = () => {
   };
 
   return (
-    <nav className="p-3 m-1 px-8 bg-gray-800 flex items-center justify-between text-white">
-      {/* Logo */}
+    <nav className="p-3 m-1 px-8 bg-white flex items-center justify-between text-gray-700 h-[70px] border-b border-gray-200 " >
+       {/* Logo */}
       <div>
         <img src={logo} alt="Logo" className="w-25 h-25 object-contain" />
       </div>
 
       {/* Logout Button and Modal */}
       <div className="relative">
-        <button
-          onClick={toggleModal}
-          className="bg-red-600 text-amber-50 px-5 py-2 rounded-md text-sm font-semibold flex items-center gap-2 shadow transition-colors hover:bg-red-800 "
-        >
-          <IoPower className="w-5 h-5  text-white" />
-          Logout
-        </button>
+      <button
+  onClick={toggleModal}
+  className="bg-red-600 text-white w-10 h-10 rounded-full flex items-center justify-center shadow hover:bg-red-800 transition-colors"
+>
+  <IoPower className="w-7 h-7" />
+</button>
+
 
         {/* Logout Confirmation Modal */}
         {showModal && (
