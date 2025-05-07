cf2a68532cf2489d9b993857372026cf91fbe5d7
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-05-07 18:22:04 +0530

    folder rearranged

--- frontend/src/route/Router.jsx
+++ frontend/src/route/Router.jsx
@@ -15,7 +15,7 @@ const HomePage = lazy(() => import('../pages/user/HomePage'));
 const LoginPage = lazy(() => import('../pages/admin/LoginPage'));
 const UserLoginPage = lazy(() => import('../pages/user/UserLoginPage'));
 const SignUpPage = lazy(() => import('../pages/user/SignUpPage'));
-const ForgetPasswordPage = lazy(() => import('../pages/user/ForgetPasswordPage'));
+const ForgotPasswordPage = lazy(() => import('../pages/user/ForgotPasswordPage'));
 const UserServicePage = lazy(() => import('../pages/user/ServicePage'));
 const ResetPassword = lazy(() => import('../components/shared/ResetPassword'));
 const UserProfilePage = lazy(() => import('../pages/user/UserProfilePage'));
@@ -66,22 +66,26 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: (
-      <GuestGuard>
-        {withSuspense(UserLoginPage)}
-      </GuestGuard>
+      <AuthProvider>
+        <GuestGuard>
+          {withSuspense(UserLoginPage)}
+        </GuestGuard>
+      </AuthProvider>
     ),
   },
   {
     path: '/signup',
     element: (
-      <GuestGuard>
-        {withSuspense(SignUpPage)}
-      </GuestGuard>
+      <AuthProvider>
+        <GuestGuard>
+          {withSuspense(SignUpPage)}
+        </GuestGuard>
+      </AuthProvider>
     ),
   },
   {
     path: '/forgot-password',
-    element: withSuspense(ForgetPasswordPage),
+    element: withSuspense(ForgotPasswordPage),
   },
   {
     path: '/reset-password',
