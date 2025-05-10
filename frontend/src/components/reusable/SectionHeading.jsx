3adfd55b075054c2e993a86d1a9b745e90952a9a
nehala-t <nehalamehak2772@gmail.com>
2025-05-10 17:11:31 +0530

    update sectionheading and create footer

--- frontend/src/components/reusable/SectionHeading.jsx
+++ frontend/src/components/reusable/SectionHeading.jsx
@@ -3,29 +3,30 @@ import React from 'react';
 const SectionHeading = ({ backgroundText, heading, subtext }) => {
   return (
     <section className=''>
- <div className="relative w-full text-center py-16 sm:py-20 lg:py-32 overflow-hidden ">
+    <div className="relative w-full text-center py-16 sm:py-20 lg:py-32 overflow-hidden">
   {/* Background large text */}
- <h1
-  className="font-coastal-clean 
+  <h1 className="font-coastal-clean 
     absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
     whitespace-nowrap
     text-[15vw] sm:text-[8vw] md:text-[11vw]
     text-gray-200 opacity-60
     uppercase tracking-wide
-    pointer-events-none select-none leading-none
-  "
->
-  {backgroundText}
-</h1>
+    pointer-events-none select-none leading-none">
+    {backgroundText.split(" ").map((word, index) => (
+      <span key={index} className="inline-block">
+        <span className="text-[125%]">{word[0]}</span>{word.slice(1)}&nbsp;
+      </span>
+    ))}
+  </h1>
 
-
-  {/* Foreground heading - visually centered */}
-  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[16%] z-10">
-    <h2 className="text-base sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-black whitespace-nowrap">
+  {/* Foreground heading - slightly moved down */}
+  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-[40%] z-10">
+    <h2 className="text-base sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-black">
       {heading}
     </h2>
   </div>
 </div>
+
 
 
 
