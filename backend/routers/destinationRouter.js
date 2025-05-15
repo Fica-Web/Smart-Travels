6d24ac5166700d699c8a12c3addc01499c82e80e
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-05-15 11:38:45 +0530

    controller function for getDestinations and get destinationById

--- backend/routers/destinationRouter.js
+++ backend/routers/destinationRouter.js
@@ -3,12 +3,16 @@ const router = express.Router();
 import verifyAdmin from '../middlewares/adminAuth.js';
 import upload from '../middlewares/multer.js';
 import {
+    getAllDestinations,
+    getDestinationById,
     createDestination,
     updateDestination,
 } from '../controllers/destinationController.js';
 
 router.use(verifyAdmin); // Ensure admin authentication for the following routes
 
+router.get('/', getAllDestinations);
+router.get('/:id', getDestinationById);
 router.post('/', upload.single('coverImage'), createDestination);
 router.put('/:id', upload.single('coverImage'), updateDestination);
 
