cbdf69ea9c323111a12f3d0104d100df0c600fdc
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-05-20 16:22:51 +0530

    section heading modified

--- frontend/src/components/reusable/SectionHeading.jsx
+++ frontend/src/components/reusable/SectionHeading.jsx
@@ -3,29 +3,29 @@ import React from 'react';
 const SectionHeading = ({ backgroundText, heading, subtext }) => {
   return (
     <section>
-      <div className="relative w-full text-center px-4 py-16 sm:py-20 lg:py-40 overflow-hidden">
-        
+      <div className="relative w-full text-center px-4 py-10 sm:py-20 lg:py-40 overflow-hidden sm:mt-0 mt-10">
+
         {/* Background large text */}
         <h1 className="font-coastal-clean absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-  whitespace-nowrap
-  text-[15vw] sm:text-[8vw] md:text-[11vw]
-  text-secondary-blue opacity-8
-  uppercase tracking-wide pointer-events-none select-none leading-none"
->
-  {backgroundText.split(" ").map((word, index) => (
-    <span key={index} className="inline-block">
-      {index === 0 ? (
-        <>
-          <span className="text-[125%]">{word[0]}</span>
-          {word.slice(1)}
-        </>
-      ) : (
-        word
-      )}
-      &nbsp;
-    </span>
-  ))}
-</h1>
+          whitespace-nowrap
+          text-[15vw] sm:text-[8vw] md:text-[11vw]
+          text-secondary-blue opacity-8
+          uppercase tracking-wide pointer-events-none select-none leading-none"
+        >
+          {backgroundText.split(" ").map((word, index) => (
+            <span key={index} className="inline-block">
+              {index === 0 ? (
+                <>
+                  <span className="text-[125%]">{word[0]}</span>
+                  {word.slice(1)}
+                </>
+              ) : (
+                word
+              )}
+              &nbsp;
+            </span>
+          ))}
+        </h1>
 
 
         {/* Foreground heading */}
