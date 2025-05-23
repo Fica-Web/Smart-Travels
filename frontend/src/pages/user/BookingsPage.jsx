04a80dfa0805e2acdd738cac11bd38eae815a8c8
nehala-t <nehalamehak2772@gmail.com>
2025-05-23 14:45:12 +0530

    merge confilict fixed

---
+++ frontend/src/pages/user/BookingsPage.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import ReusableHero from '../../components/reusable/ReusableHero';
+import TopIcons from '../../components/HomeSection/BookingPage/TopIcons';
+import img from '../../assets/image/booking/booking-hero.jpg';
+
+const BookingPage = () => {
+  return (
+    <div>
+      <ReusableHero title="Start Your Journey – Book Now" bgImage={img} />
+      <div className="relative md:-mt-8 mt-5 md:z-10">
+        <TopIcons />
+      </div>
+      {/* Render the child route component here */}
+      <div className='py-10 px-6 md:px-20'>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default BookingPage;
