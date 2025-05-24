a83b4efd8cb4467c7116273617cefb3401e65209
nehala-t <nehalamehak2772@gmail.com>
2025-05-24 14:30:27 +0530

    Add TripDetails to DestinationDetails page and refactor ContactForm to be reusable for Contact and DestinationDetails pages

--- frontend/src/components/HomeSection/BookingPage/DestinationDetails.jsx
+++ frontend/src/components/HomeSection/BookingPage/DestinationDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDestinationByIdApi } from '../../../services/api/destinationApi';
 import { IoFlagSharp } from "react-icons/io5";
-import ItinerarySection from './ItinerarySection';
+import TripDetails from './TripDetails';
 
 const DestinationDetails = () => {
     const { id } = useParams();
@@ -11,11 +11,11 @@ const DestinationDetails = () => {
 
     useEffect(() => {
         const fetchDestination = async () => {
-            console.log('Fetching destination with ID:', id); 
+            console.log('Fetching destination with ID:', id);
 
             const response = await getDestinationByIdApi(id);
 
-            console.log('API response:', response); 
+            console.log('API response:', response);
 
             if (response.success) {
                 setDestination(response.data.destination);
@@ -34,30 +34,35 @@ const DestinationDetails = () => {
     if (!destination) return <p>Destination not found.</p>;
 
     return (
-        <div className='md:px-20 px-5 '>
-        <div className="pt-6 text-secondary-blue">
-            <img
-                src={destination.coverImage}
-                alt={destination.title}
-                className="w-full h-auto max-h-[575px] rounded-3xl object-cover"
-            />
-            <div className="flex flex-col md:flex-row justify-between items-start md:items-center text-secondary-blue gap-2 mt-4">
-                {/* Title */}
-                <h1 className="text-2xl font-bold">{destination.title}</h1>
+        <div className='px-0 md:px-20  '>
+            <div className="pt-3 md:pt-6 text-secondary-blue">
+                <img
+                    src={destination.coverImage}
+                    alt={destination.title}
+                    className="w-full h-auto md:max-h-[575px] rounded-3xl object-cover"
+                />
+                <div className="flex flex-col md:flex-row justify-between items-start md:items-center text-secondary-blue gap-3 mt-4">
+                    {/* Title */}
+                    <h1 className="text-2xl font-bold">{destination.title}</h1>
 
-                {/* Duration and Country */}
-                <div className="flex items-center space-x-2 text-sm md:text-base ">
-                    <span>{destination.duration}</span>
-                    <span className="flex items-center gap-1">
-                        ,<IoFlagSharp size={16} />
-                        {destination.country}
-                    </span>
+                    {/* Duration and Country */}
+                    <div className="flex items-center  text-sm md:text-base ">
+                        <span>{destination.duration}</span>
+                        <span className="flex items-center gap-1">
+                            ,<IoFlagSharp size={16} />
+                            {destination.country}
+                        </span>
+                    </div>
+                </div>
+                <p className='text-sm sm:text-base text-secondary-blue/80 mt-4 md:mt-6 text-justify '>{destination.overview}</p>
+            </div>
+            <div className='w-full flex flex-col lg:flex '>
+                <div className='md:w-1/2'>
+                    <TripDetails destination={destination} />
+
                 </div>
             </div>
-            <p className='text-sm sm:text-base text-secondary-blue/80'>{destination.overview}</p>
-        </div>
 
-        <ItinerarySection destination={destination} />
         </div>
     );
 };
---
+++ frontend/src/components/HomeSection/BookingPage/Tripdetails.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { IoLocationSharp } from "react-icons/io5";
+import { IoMdCheckmark } from "react-icons/io";
+import ContactForm from '../../reusable/ContactForm';
+
+
+
+const TripDetails = ({ destination }) => {
+  return (
+    <div className='flex flex-col gap-4 ' >
+      <ItinerarySection destination={destination} />
+      <Inclusions destination={destination} />
+
+
+    </div>
+  )
+}
+
+export default TripDetails
+
+const ItinerarySection = ({ destination }) => {
+  return (
+    <div className='pt-6 text-secondary-blue '>
+      <h1 className='text-xl font-bold text-secondary-blue'>Day-by-Day Itinerary</h1>
+
+      <div className='pt-6 '>
+        <div className="relative">
+          {destination.days?.map((day, index) => (
+            <div key={index} className="pb-6 relative pl-6 ">
+              {/* Left vertical dashed line - hide for last element */}
+              {index !== destination.days.length - 1 && (
+                <div className="absolute left-1 top-1 bottom-0 w-px border-l border-dashed border-gray-400 z-0" />
+              )}
+
+              {/* Icon on top of the border */}
+              <div className="absolute left-[-6px] top-1 z-10 bg-white rounded-full">
+                <IoLocationSharp size={20} className="text-secondary-blue" />
+              </div>
+
+              {/* Content */}
+              <div className="relative z-10 text-secondary-blue">
+                <h2 className="text-lg font-semibold">
+                  Day {index + 1}: {day.title}
+                </h2>
+                <p className="text-sm sm:text-base text-secondary-blue/80 max-w-2xl text-justify ">{day.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+const Inclusions = ({ destination }) => {
+  console.log("Inclusions array:", destination.inclusions); 
+
+  return (
+    <div className="p-4 text-secondary-blue md:max-w-[337.6px]">
+  <h2 className="text-lg font-bold mb-2">Inclusions</h2>
+  <div className="grid grid-cols-2 gap-x-0 gap-y-1">
+    {destination.inclusions?.map((inclusion, index) => (
+      <div key={index} className="flex items-center gap-1">
+        <IoMdCheckmark size={16} className="shrink-0" />
+        <p className="text-sm">{inclusion}</p>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+  );
+};
+
+
