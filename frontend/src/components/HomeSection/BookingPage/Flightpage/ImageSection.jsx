50daa8df00b1d8aac6a43c783d354a4b96fda525
nehala-t <nehalamehak2772@gmail.com>
2025-05-31 11:13:01 +0530

    adjust layout of ImageSection for better responsiveness on flight booking page

--- frontend/src/components/HomeSection/BookingPage/Flightpage/ImageSection.jsx
+++ frontend/src/components/HomeSection/BookingPage/Flightpage/ImageSection.jsx
@@ -13,7 +13,7 @@ const ImageSection = () => {
   };
 
   return (
-    <div className="relative w-full inline-block mt-10 overflow-hidden ">
+    <div className="relative w-full inline-block mt-10 overflow-hidden">
       {/* Image */}
       <img src={Img} alt="" className="w-full h-auto rounded-3xl " />
 
@@ -74,39 +74,52 @@ const BottomOverlayBox = ({ onSubmit }) => {
 
 
   return (
-    <div className="absolute md:bottom-8 left-1/2 -translate-x-1/2 md:bg-white/30 border md:border-white/20 md:backdrop-blur-[20px] md:shadow-[inset_0px_2px_6px_0px_#0000001A] py-4 rounded-2xl z-10 w-full max-w-5xl flex flex-wrap justify-center items-center md:gap-20">
-      {fields.map(({ label, placeholder, icon, type, name }) => (
-        <div
-          key={name}
-          className="flex items-center gap-0 w-full sm:w-[180px] rounded-xl"
-        >
-          {/* Icon */}
-          <div className="min-w-[44px] h-[44px] flex items-center justify-center rounded-full bg-[#4A94D0]">
-            <img src={icon} alt={label} className="w-6 h-6 object-contain" />
-          </div>
+<div className="w-full  md:max-w-2xl lg:max-w-5xl 
+  grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 md:items-center md:justify-items-center lg:flex 
+  justify-center items-center gap-4 md:gap-x-4 md:gap-y-3 lg:gap-10 
+  py-4 md:py-2 lg:py-4 rounded-2xl z-10 
+  md:absolute md:bottom-1 lg:bottom-8 md:left-1/2 md:-translate-x-1/2 
+  md:bg-white/30 md:border md:border-white/20 md:backdrop-blur-[20px] md:shadow-[inset_0px_2px_6px_0px_#0000001A] 
+  bg-white"
+>
+  {fields.map(({ label, placeholder, icon, type, name }, index) => (
+   <div
+  key={name}
+  className={`flex items-center gap-2 min-w-[220px] w-full sm:w-[220px] 
+    ${index === 2 ? 'md:col-start-1' : ''} 
+    rounded-xl`}
+>
+  {/* Icon */}
+  <div className="min-w-[44px] min-h-[44px] md:min-w-[40px] md:min-h-[40px] lg:min-w-[44px] lg:min-h-[44px] flex items-center justify-center rounded-full bg-[#4A94D0]">
+    <img src={icon} alt={label} className="w-6 h-6 object-contain" />
+  </div>
 
-          {/* Label and Input */}
-          <div className="flex flex-col w-full px-2 text-left">
-            <label className="text-sm text-secondary-blue">{label}</label>
-            <input
-              type={type}
-              placeholder={placeholder}
-              name={name}
-              value={formData[name]}
-              onChange={handleChange}
-              className="text-sm  bg-transparent outline-none placeholder:text-secondary-blue text-secondary-blue"
-            />
-          </div>
-        </div>
-      ))}
 
-      <button
-        onClick={handleSubmit}
-        className="bg-[#4A94D0] text-white font-semibold px-9 py-2 rounded-md hover:bg-[#3B7DB1] transition w-full sm:w-auto"
-      >
-        Send Query
-      </button>
+      {/* Label and Input */}
+      <div className="flex flex-col w-full px-1 lg:px-2 text-left">
+        <label className="text-sm text-secondary-blue">{label}</label>
+        <input
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          value={formData[name]}
+          onChange={handleChange}
+          className="text-sm bg-transparent outline-none placeholder:text-secondary-blue text-secondary-blue"
+        />
+      </div>
     </div>
+  ))}
+
+  <button
+    onClick={handleSubmit}
+    className="bg-[#4A94D0] text-white font-semibold px-9 md:px-5 lg:px-9 py-2 rounded-md hover:bg-[#3B7DB1] transition w-full md:w-[190px] lg:w-auto"
+  >
+    Send Query
+  </button>
+</div>
+
+
+
   );
 };
 
