9c8e1f9863055a8e6567350368ae7b4ac51a1797
nehala-t <nehalamehak2772@gmail.com>
2025-05-31 14:29:59 +0530

    Make FlightEndSection responsive for mobile and tablet views

--- frontend/src/components/HomeSection/BookingPage/Flightpage/FlightEndSection.jsx
+++ frontend/src/components/HomeSection/BookingPage/Flightpage/FlightEndSection.jsx
@@ -4,16 +4,18 @@ import img from '../../../../assets/image/booking/flight/flightendimg.png'
 const FlightEndSection = () => {
   return (
     <div className="relative w-full py-2">
-      <img src={img} alt="Flight Background" className="w-full h-auto object-cover" />
+  <img src={img} alt="Flight Background" className="w-full h-auto object-cover" />
 
-      <div className="absolute top-1/2 left-8 transform -translate-y-1/2 text-white max-w-lg px-10">
-        <h2 className="font-bold text-[34px] leading-[42px] tracking-normal capitalize pb-3">Your Journey Starts with Comfort & Confidence</h2>
-        <p className="text-lg leading-[28px] max-w-lg ">
-  We make flight bookings smooth, secure,<br /> and budget-friendly — so you can focus on <br />the adventure ahead.
-</p>
+  <div className="absolute top-11 md:top-1/2 left-3 md:left-1 lg:left-8 transform md:-translate-y-1/2 text-white max-w-md md:max-w-lg md:px-5 lg:px-10">
+<h2 className="font-bold text-sm md:text-[34px] leading-tight md:leading-[42px]  capitalize pb-1 md:pb-3 max-w-[20ch] md:max-w-[36ch]">
+      Your Journey Starts with Comfort & Confidence
+    </h2>
+<p className="hidden sm:block text-lg leading-[28px] max-w-[40ch] ">
+      We make flight bookings smooth, secure, and budget-friendly — so you can focus on the adventure ahead.
+    </p>
+  </div>
+</div>
 
-      </div>
-    </div>
   )
 }
 
